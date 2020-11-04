5a0ba530e3f5fa6f2795f5c3d04d90da528bec99
edubugras <edubugras@gmail.com>
2020-11-03 23:09:53 -0300

    Update da fase

--- Nosferatu Hurricane/Assets/Scripts/PsychicControl.cs
+++ Nosferatu Hurricane/Assets/Scripts/PsychicControl.cs
@@ -8,17 +8,11 @@ public class PsychicControl : MonoBehaviour
     public bool activateTimer;
     public float timer;
 
-    public BloodBar bloodBar;
-
-    public RatControl ratControl;
-    public ShadowControl shadowControl;
-
     // Start is called before the first frame update
     void Start()
     {
         psychic = GameObject.Find("Psychic");
         psychic.SetActive(false);
-        bloodBar = FindObjectOfType<BloodBar>();
     }
 
     // Update is called once per frame
@@ -29,27 +23,16 @@ public class PsychicControl : MonoBehaviour
             timer += Time.deltaTime;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && timer < 2 && !activateTimer && !ratControl.ControlRat && !shadowControl.ControlShadow && bloodBar.BloodLeft > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && timer < 2)
         {
             psychic.SetActive(true);
             activateTimer = true;
-            StartCoroutine("PsychicBloodDrain");
         }
         else if(timer >= 2)
         {
             psychic.SetActive(false);
             activateTimer = false;
             timer = 0;
-            StopCoroutine("PsychicBloodDrain");
-        }
-    }
-
-    public IEnumerator PsychicBloodDrain()
-    {
-        while (true)
-        {
-            bloodBar.BloodLeft -= 0.1f;
-            yield return new WaitForSeconds(0.01f);
         }
     }
 }
